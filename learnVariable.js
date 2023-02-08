29c230edf0d2f0073d368212980da171dc4e6d88
mnguyen203 <minhnd1101@gmail.com>
2023-02-08 14:53:03 +0700

    Nodejs
    add learn variable

---
+++ learnVariable.js
@@ -0,0 +1,20 @@
+// Syntax
+// let tenBien 
+// let tenBien = 1 - da gan gia tri = 1
+
+let myNumber = 1 // camel case
+console.log("myNumber:", myNumber)
+
+myNumber = 2
+console.log("myNumber:", myNumber)
+
+// String 
+
+//let myString = 'toi ten "Minh"'  - 1 trong 2 cach 
+let myString = "toi ten \"Minh\""
+console.log(myString);
+
+// boolean true or false
+let isRight = true
+let isWrong = false
+
